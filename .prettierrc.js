16f9436d05bdeaf3179a5cefdf8324aca362ebf4
chanwukim <chnwu.kim@gmail.com>
2025-08-15 19:30:22 +0900

    init

---
+++ .prettierrc.js
@@ -0,0 +1,33 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | TailwindConfig | SortImportsConfig } */
+const config = {
+  semi: true,
+  singleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  endOfLine: "lf",
+  printWidth: 100,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["cn", "clsx", "cva", "cx"],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.9.0",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@repo/(.*)$",
+    "",
+    "^@/",
+    "^[../]",
+    "^[./]",
+  ],
+};
+
+export default config;
